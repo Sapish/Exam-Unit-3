178cf5b1edf5039f067e06b8c33bf67068632db4
Oskar <oskardb@uia.no>
2025-03-24 21:52:54 +0100

    Tried submitting SILVER as answer, didnt work

--- main.mjs
+++ main.mjs
@@ -10,26 +10,42 @@ const PlanetarySymbolsConverter = code => {
       "☽": "Silver",
       "♂": "Iron"
     };
-    return [...code].map(symbol => mapping[symbol]);
+    return [...code].map(symbol => ConvertSymbolsToMetal[symbol]);
   };
 
 (async function() {
-    const startUrl = `${ALCHEMY_API}start?player=${encodeURIComponent(playerId)}`;
-    const startResponse = await fetch(startUrl);
-    const startData = await startResponse.json();
-    console.log("Challenge started:", startData);
+    const startUrl1 = `${ALCHEMY_API}start?player=${encodeURIComponent(playerId)}`;
+    const startResponse1 = await fetch(startUrl1);
+    const startData1 = await startResponse1.json();
+    console.log("Challenge one started:", startData1);
 
-const solutionOne = ["Gold", "Quicksilver", "Silver", "Iron", "Gold"];
+const symbolsToConvert = "☉☿☽♂☉";
+const answer1 = PlanetarySymbolsConverter(symbolsToConvert);
+console.log("Converted answer for challenge one: ", answer1);
 
-const answerResponse = await fetch(`${ALCHEMY_API}answer`, {
+const answerResponse1 = await fetch(`${ALCHEMY_API}answer`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-        player: playerId,
-        answer: solutionOne
-    })
+    body: JSON.stringify({ player: playerId, answer: answer1 })
 });
 
-const answerData = await answerResponse.json();
-console.log("Answer submitted:", answerData);
+const answerData1 = await answerResponse1.json();
+console.log("Answer submitted for challenge one: ", answerData1);
+
+//-------------------------------------Task 2
+const startUrl2 = `${ALCHEMY_API}start?player=${encodeURIComponent(playerId)}`;
+    const startResponse2 = await fetch(startUrl2);
+    const startData2 = await startResponse2.json();
+    console.log("Challenge two started:", startData2);
+
+    const answer2 = "SILVER";
+    console.log("Answer for challenge two: ", answer2);
+
+    const answerResponse2 = await fetch(`${ALCHEMY_API}answer`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ player: playerId, answer: answer1 })
+    });
+    const answerData2 = await answerResponse2.json();
+    console.log("Answer submitted for challenge two: ", answerData2);
 })();
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 Imported some of my old code to call the api, and got the challenge.
 
+Task 1 -------------------
 First challenge received, crack the code: “☉☿☽♂☉”.
 After copy pasting the symbols into google i found that they are "planetary symbols" used in astrology and astronomy: https://en.wikipedia.org/wiki/Planetary_symbols
 Since the task is about alchemy, that probably is an indication that i will need to link the symbols to the metals. So my answer would be a list of the metals that match the symbols.
@@ -8,4 +9,12 @@ I believe im writing the answer correct, but im still getting wrong answer? "Gol
 Fixed the answer, i was not returning an array.
 I have now solved the task manually and got the correct answer, i will now try implementing code to solve the challenge.
   1. Added code to decode the symbols over to metals.
-  2. 
+  2. “☉☿☽♂☉” should now be converted into an array by connecting each symbol to the correct metal.
+For some reason im getting task two, even though i still haven't completed task 1, i can also no longer see task 1.
+Going to start on task two and then ill come back to this task and try figuring out the issue, honestly don't know if its a server thing or me.
+
+Task 2 -------------------
+Im guessing there is some sort of secret message within the poem “Still flows the Icy Lethe, Veiling all ’neath Eldritch Rime.”
+1. First thing i thought of was that it could be the numbers of letters in the poem, but that wouldn't really be a secret message but a number.
+2. Second thought if it had to be a secret message is trying the capital letters, so ill make a test for that.
+
